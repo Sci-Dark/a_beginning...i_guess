c460c3fe7051f301b17fb2f81c5d9f50cee307e4
Sci-Dark <94867823+Sci-Dark@users.noreply.github.com>
2021-11-22 11:56:25 -0800

    cool program i made in 2 minutes
    yea i was bored and i wanna see how github works so enjoy this Java program i made in 2 minutes

---
+++ coolprogram.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+  static Scanner inp = new Scanner();
+  static String lang = inp.nextLine();
+  
+  public static void main(String[ ] args) {
+    System.out.println("Hello from " + lang);
+  }
+}
